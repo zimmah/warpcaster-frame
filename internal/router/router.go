297b4c5717066b394ac7a76d0b185e49b241726e
zimmah <zimmah@gmail.com>
2024-08-26 17:57:42 -0500

    listen to port

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,8 +1,22 @@
 package router
 
-import "net/http"
+import 
+(
+	"net/http"
+	"os"
+	"log"
+)
 
 func Router() {
 	mux := http.NewServeMux()
 	mux.Handle("GET /api/frames/{frameID}", logger(http.HandlerFunc(handleGetFrameByID)))
+
+	port := os.Getenv("PORT")
+	server := &http.Server{
+		Addr: 	":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Listening on port: %s", port)
+	log.Fatal(server.ListenAndServe())
 }
