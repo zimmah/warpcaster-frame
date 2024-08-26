8d0c16251d1d0c411d9e67bc915b9eabb5a9d1f8
zimmah <zimmah@gmail.com>
2024-08-26 18:17:15 -0500

    attempt sending response frame

--- internal/router/frames.go
+++ internal/router/frames.go
@@ -6,14 +6,64 @@ import (
 	"fmt"
 )
 
+const placeholderFrame = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>Apple frame</title>
+    <meta property="fc:frame" name="fc:frame" content="vNext" />
+    <meta
+      property="fc:frame:image"
+      name="fc:frame:image"
+      content="https://fastly.picsum.photos/id/237/300/300.jpg"
+    />
+    <meta
+      property="fc:frame:image:aspect_ratio"
+      name="fc:frame:image:aspect_ratio"
+      content="1:1"
+    />
+    <meta
+      property="og:image"
+      name="og:image"
+      content="https://fastly.picsum.photos/id/237/300/300.jpg"
+    />
+    <meta
+      name="fc:frame:post_url"
+      content="https://frames-bevm.onrender.com/"
+    />
+    <meta name="fc:frame:button:1" content="Go to Tribally" />
+    <meta name="fc:frame:button:1:action" content="link" />
+    <meta name="fc:frame:button:1:target" content="https://tribally.games" />
+    <meta name="fc:frame:button:2" content="Show next frame" />
+    <meta name="fc:frame:button:2:action" content="post" />
+    <meta
+      name="fc:frame:button:2:target"
+      content="https://frames-bevm.onrender.com/api/frames/2"
+    />
+    <link href="style.css" rel="stylesheet" />
+  </head>
+  <body>
+    <h1>this is a different frame, with an dog</h1>
+    <img
+      src="https://fastly.picsum.photos/id/237/300/300.jpg"
+    />
+  </body>
+</html>
+`
+
 func handleGetFrameByID(w http.ResponseWriter, r *http.Request) {
-	frameID, err := strconv.Atoi(r.PathValue("frameID"))
+	_, err := strconv.Atoi(r.PathValue("frameID"))
 	if err != nil { 
 		respondWithError(w, http.StatusBadRequest, "Invalid frame ID")
 		return
 	}
 
-	fmt.Println(frameID)
-	w.WriteHeader(200)
-	// got to serve a new frame by ID
+	fmt.Print(r)
+	w.Header().Set("Content-Type", "text/html")
+	statusCode, err := w.Write([]byte(placeholderFrame))
+	if err != nil {
+		respondWithError(w, statusCode, "Couldn't generate frame")
+		return
+	}
+	w.WriteHeader(statusCode)
 }
